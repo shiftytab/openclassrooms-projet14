3cbf94eb14847b6c09dfd5730e0f49bc35e4fc40
shiftytab <rodrigoestevesramos@gmail.com>
2023-12-27 17:47:05 +0100

    Add EmployeeType

---
+++ src/types/EmployeeType.ts
@@ -0,0 +1,11 @@
+export type EmployeePropsType = {
+    firstName: string;
+    lastName: string;
+    dateOfBirth: string | Date;
+    startDate: string | Date;
+    department: string;
+    street: string;
+    city: string;
+    employeeState: string;
+    zipCode: string;
+}
