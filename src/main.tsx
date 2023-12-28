d9d67e1f1e54e8a4b78536e5fb73f8997dfc0851
Rodrigo Ramos <42652709+shiftytab@users.noreply.github.com>
2023-12-28 09:49:01 +0100

    Update Router to createBrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -4,19 +4,28 @@ import store from '@/store/index';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import "primereact/resources/themes/lara-light-cyan/theme.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <List />
+  },
+  {
+    path: "/new",
+    element: <NewElement />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<List />} />
-          <Route path="/new" element={<NewElement />} />
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
 );
